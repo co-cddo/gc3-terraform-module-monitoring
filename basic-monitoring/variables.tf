e55213d57c3e054cd49620db096392eaa71ba89d
“tolga-cddo” <seyit.tolga@digital.cabinet-office.gov.uk>
2024-10-30 08:44:22 +0000

    Add basic monitoring terraform

---
+++ basic-monitoring/variables.tf
@@ -0,0 +1,54 @@
+variable "health_check_name" {
+  description = "Name tag for the health check"
+  type        = string
+  default     = ""
+}
+
+variable "fqdn" {
+  description = "The FQDN of the health check"
+  type        = string
+}
+
+variable "port" {
+  description = "The port number for the health check"
+  type        = number
+}
+
+variable "resource_path" {
+  description = "The resource path for the health check"
+  type        = string
+}
+
+variable "request_interval" {
+  description = "The request interval for the health check"
+  type        = number
+  default     = 30
+}
+
+variable "failure_threshold" {
+  description = "The failure threshold for the health check"
+  type        = number
+  default     = 1
+}
+
+variable "search_string" {
+  description = "The search string for the health check"
+  type        = string
+  default     = ""
+}
+
+variable "health_check_regions" {
+  description = "Regions where the health check is monitored"
+  type        = list(string)
+  default     = ["us-west-1", "us-west-2", "eu-west-1"]
+}
+
+variable "email" {
+  description = "The email to send alerts to"
+  type        = string
+}
+
+locals {
+  service_name   = "webcaf"
+  service_name_w = "webcaf-${terraform.workspace}"
+}
