bd59bab1259b6a5a0051fb35d0aa335f5dc712f0
“tolga-cddo” <seyit.tolga@digital.cabinet-office.gov.uk>
2024-11-10 15:20:17 +0000

    MUltiple endpoints

--- basic-monitoring/main.tf
+++ basic-monitoring/main.tf
@@ -1,21 +1,3 @@
-resource "aws_route53_health_check" "health_check" {
-  fqdn              = var.fqdn
-  type              = "HTTPS_STR_MATCH"
-  port              = var.port
-  resource_path     = var.resource_path
-  request_interval  = var.request_interval
-  failure_threshold = var.failure_threshold
-  search_string     = var.search_string
-  regions           = var.health_check_regions
-  # cloudwatch_alarm_name = aws_cloudwatch_metric_alarm.health_check_alarm.name
-  # cloudwatch_alarm_region = "eu-west-1"  
-
-  tags = {
-    Name =  "${local.service_name_w}-health-check"
-  }
-
-}
-
 resource "aws_sns_topic" "alarm_topic" {
   name = "${local.service_name_w}-health-check-alerts"
 }
@@ -26,9 +8,92 @@ resource "aws_sns_topic_subscription" "email_subscription" {
   endpoint  = var.email
 }
 
+# resource "aws_route53_health_check" "health_check" {
+#   fqdn              = var.fqdn
+#   type              = "HTTPS_STR_MATCH"
+#   port              = var.port
+#   resource_path     = var.resource_path
+#   request_interval  = var.request_interval
+#   failure_threshold = var.failure_threshold
+#   search_string     = var.search_string
+#   regions           = var.health_check_regions
+#   tags = {
+#     Name =  "${local.service_name_w}-health-check"
+#   }
+
+# }
+
+
+# health checks for HTTPS_STR_MATCH
+resource "aws_route53_health_check" "health_check_str_match" {
+  for_each = { for i, endpoint in var.endpoints : i => endpoint if endpoint.type == "HTTPS_STR_MATCH" }
+
+  fqdn              = var.fqdn
+  type              = "HTTPS_STR_MATCH"
+  port              = var.port
+  resource_path     = each.value.resource_path
+  request_interval  = each.value.request_interval
+  failure_threshold = each.value.failure_threshold
+  search_string     = each.value.search_string
+  regions           = var.health_check_regions
+
+  tags = {
+    Name = "${local.service_name_w}-health-check-${each.key}"
+  }
+}
+
+# health checks for HTTPS
+resource "aws_route53_health_check" "health_check_https" {
+  for_each = { for i, endpoint in var.endpoints : i => endpoint if endpoint.type == "HTTPS" }
+
+  fqdn              = var.fqdn
+  type              = "HTTPS"
+  port              = var.port
+  resource_path     = each.value.resource_path
+  request_interval  = each.value.request_interval
+  failure_threshold = each.value.failure_threshold
+  regions           = var.health_check_regions
+
+  tags = {
+    Name = "${local.service_name_w}-health-check-${each.key}"
+  }
+}
+
+
+# resource "aws_cloudwatch_metric_alarm" "health_check_alarm" {
+#   alarm_name          = "${local.service_name_w}-health-check-alarm"
+#   comparison_operator = "LessThanThreshold"
+#   evaluation_periods  = 1
+#   metric_name         = "HealthCheckStatus"
+#   namespace           = "AWS/Route53"
+#   period              = 60
+#   statistic           = "Minimum"
+#   threshold           = 1
+#   alarm_description   = "This metric monitors route 53 healthchecks"
+#   actions_enabled     = "true"
+#   alarm_actions = [
+#     aws_sns_topic.alarm_topic.arn
+#   ]
+#   ok_actions = [
+#     aws_sns_topic.alarm_topic.arn
+#   ]
+#   # treat_missing_data  = "breaching"
+#   dimensions = {
+#       HealthCheckId = aws_route53_health_check.health_check.id
+#    }
+#   depends_on = [
+#      aws_route53_health_check.health_check,
+#      aws_sns_topic.alarm_topic
+#     ]
+# }
 
 resource "aws_cloudwatch_metric_alarm" "health_check_alarm" {
-  alarm_name          = "${local.service_name_w}-health-check-alarm"
+  for_each = merge(
+    aws_route53_health_check.health_check_str_match,
+    aws_route53_health_check.health_check_https
+  )
+
+  alarm_name          = "${local.service_name_w}-health-check-alarm-${each.key}"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = 1
   metric_name         = "HealthCheckStatus"
@@ -36,20 +101,17 @@ resource "aws_cloudwatch_metric_alarm" "health_check_alarm" {
   period              = 60
   statistic           = "Minimum"
   threshold           = 1
-  alarm_description   = "This metric monitors route 53 healthchecks"
-  actions_enabled     = "true"
-  alarm_actions = [
-    aws_sns_topic.alarm_topic.arn
-  ]
-  ok_actions = [
-    aws_sns_topic.alarm_topic.arn
-  ]
-  # treat_missing_data  = "breaching"
+  alarm_description   = "This metric monitors route 53 healthchecks for ${var.fqdn} at ${each.value.resource_path}"
+  actions_enabled     = true
+  alarm_actions       = [aws_sns_topic.alarm_topic.arn]
+  ok_actions          = [aws_sns_topic.alarm_topic.arn]
+
   dimensions = {
-      HealthCheckId = aws_route53_health_check.health_check.id
-   }
+    HealthCheckId = each.value.id
+  }
+
   depends_on = [
-     aws_route53_health_check.health_check,
-     aws_sns_topic.alarm_topic
-    ]
+    aws_route53_health_check.health_check_str_match,
+    aws_route53_health_check.health_check_https
+  ]
 }
--- basic-monitoring/variables.tf
+++ basic-monitoring/variables.tf
@@ -48,6 +48,20 @@ variable "email" {
   type        = string
 }
 
+
+variable "endpoints" {
+  description = "List of endpoint configurations"
+  type = list(object({
+    resource_path     = string
+    search_string     = string
+    request_interval  = number
+    failure_threshold = number
+    type              = string  # Either "HTTPS" or "HTTPS_STR_MATCH"
+  }))
+}
+
+
+
 locals {
   service_name   = "webcaf"
   service_name_w = "webcaf-${terraform.workspace}"
